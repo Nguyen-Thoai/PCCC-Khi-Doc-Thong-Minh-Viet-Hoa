5a483f3f43b540564c0fafb03933922f5f09c48a
Nguyên Thoại <dnthoai11@gmail.com>
2025-04-07 17:23:30 +0700

    Create config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+// Your WiFi credentials.
+
+#define ssid      "ESP32"             //AP SSID
+#define pass      ""                //AP Password
+
+String  Essid = "";                 //EEPROM Network SSID
+String  Epass = "";                 //EEPROM Network Password
+String  Etoken = "";                //EEPROM Network token
+
+String  sssid = "";                 //Read SSID From Web Page
+String  passs = "";                 //Read Password From Web Page
+String  token = "";                 //Read token From Web Page
